32f58990ad92f36441c3033443c67e233ad692fb
lsparko <akojurudaniel@gmail.com>
2022-12-08 04:58:21 -0800

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/**
- * main - entry point
- * return: the output value
- */
+/*
+ * main - print a line of code  using puts
+ *
+ * return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-return(0);
+        return (0);
 }
