fcf28f6eebbf1b9867f76e8dfa8f77ce759480cc
lsparko <akojurudaniel@gmail.com>
2022-12-08 07:31:51 -0800

    print size of all data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print a line of code
+ * Return: 0
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %lu byte(S)\n", sizeof(a));
+	printf("Size of an int: %lu byte(S)\n", sizeof(b));
+	printf("Size of a long int: %lu byte(S)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(S)\n", sizeof(d));
+	printf("Size of a float: %lu byte(S)\n", sizeof(f));
+	return (0);
+}
