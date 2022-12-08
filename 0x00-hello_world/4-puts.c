87d51d602ac06b307358fda20ed0358962073f72
lsparko <akojurudaniel@gmail.com>
2022-12-08 06:02:21 -0800

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
  * main - print a line of code
- * description: print "\"Programming is like building a multilingual puzzle"
- * return: 0
-*/
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
