36288c59e19dc00a31353279f5107acb651dc07b
lsparko <akojurudaniel@gmail.com>
2022-12-13 06:35:14 -0800

    compute absolut value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - print absolute value of integer
+ * @n: integer to be printed
+ *
+ * Return: absolute value of integer
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
