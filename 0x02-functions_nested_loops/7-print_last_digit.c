a599d8d4a7eb3c83756b29bc8224b9454875d2b0
lsparko <akojurudaniel@gmail.com>
2022-12-13 07:07:45 -0800

    find last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main."
+/**
+ * print_last_digit - print last digit of a number
+ * @n: number to be computed
+ *
+ * Return: value of number
+*/
+
+int print_last_digit(int n)
+{
+	lastdigit = n % 10;
+	if (lastdigit < 0)
+		lastdigit *= -1;
+	_putchar(lastdigit + '0');
+	return (lastdigit);
+}
