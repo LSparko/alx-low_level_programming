2d4703fb0e32518f43e0a40b10e1ef1380228bdd
Lsparko <Akojurudaniel@gmail.com>
2022-12-19 14:13:41 -0800

    fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,15 +9,17 @@
  * Return: pointer to the destination string @dest
 */
 
-char *_strcpy(char *dest,const char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	while (src[index])
+	while (src[index] != '\0')
 	{
 		dest[index] = src[index];
 		index++;
 	}
-	
+	while (index++)
+		dest[index] = '\0';
+
 	return (dest);
 }
