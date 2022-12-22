fecaa4a188cb6fb824e09ff322bb1889c4be356c
Lsparko <Akojurudaniel@gmail.com>
2022-12-22 08:58:48 -0800

    fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,20 +12,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int s1_len = 0, s2_len = 0;
-
-	while (*s1++)
-		s1_len++;
-
-	while (*s2++)
-		s2_len++;
-
-	if (s1_len > s2_len)
-		return (15);
-
-	else if (s1_len < s2_len)
-		return (-15);
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
 
 	else
-		return (0);
+		return (*s1 - *s2);
 }
