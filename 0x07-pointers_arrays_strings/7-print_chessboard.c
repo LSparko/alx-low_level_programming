515803dcd40b99d519820327b3da17199f14f511
Lsparko <Akojurudaniel@gmail.com>
2023-01-02 13:10:06 -0800

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints chessboard
+ * @a: chessboard to be printed
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int h1, h2;
+
+	for (h1 = 0; a[h1][7]; h1++)
+	{
+		for (h2 = 0; h2 < 8; h2++)
+			_putchar(a[h1][h2]);
+
+		_putchar('\n');
+	}
+}
