19945384723a78db4c601daeaa917ff2b09feb87
Lsparko <Akojurudaniel@gmail.com>
2023-01-16 03:08:55 -0800

    struct,typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+ * free_dog - frees struct dog
+ * @d: dog to be freed
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
