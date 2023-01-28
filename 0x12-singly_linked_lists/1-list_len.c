eaa6c033b1cc560851d44a2cd74ceba821409da4
LSparko <Akojurudaniel@gmail.com>
2023-01-27 20:26:42 -0800

    betty fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,12 +11,12 @@
 size_t list_len(const list_t *h)
 {
 	size_t elements = 0;
-		while (h)
-		{
-		elements++;
 
+	while (h)
+	{
+		elements++;
 		h = h->next;
-		}
-	
-		return (elements);
+	}
+
+	return (elements);
 }
