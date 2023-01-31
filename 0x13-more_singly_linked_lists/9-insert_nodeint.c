8d3cfd335cd2300a9b65f75c8668d9fa4ce52248
LSparko <Akojurudaniel@gmail.com>
2023-01-30 23:54:58 -0800

    betty fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,9 +18,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->n = n;
-	
+
 	if (idx == 0)
 	{
 		new_node->next = cpy;
@@ -32,6 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (cpy == NULL || cpy->next == NULL)
 			return (NULL);
+
 		cpy = cpy->next;
 	}
 
