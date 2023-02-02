539453bcf18ba4b3c0a71a23ee4b9a9c488e2561
LSparko <Akojurudaniel@gmail.com>
2023-02-02 13:49:21 -0800

    added header file for null

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int
@@ -14,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int a = 0, j = 1;
 	int len;
 
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
 
 	for (len = 0; b[len];)
